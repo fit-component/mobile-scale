9f4b8a482f6a820ea9baf91f3a6688c32ebca7f5
huangziyi01 <huangziyi01@baidu.com>
2016-05-26 16:09:48 +0800

    quick push

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-const scale = (width:number = 400, reactDomName:string = 'react-dom')=> {
+/// <reference path="../../../../typings-module/process.d.ts" />
+
+import * as process from 'process'
+
+const scale = (width: number = 400, reactDomName: string = 'react-dom')=> {
     // 设置页面缩放~
-    const WIDTH = width
-    const ratio = screen.width / WIDTH
-    const meta = document.createElement('meta')
-    meta.setAttribute('name', 'viewport')
-    meta.setAttribute('content', 'width=' + WIDTH + ',initial-scale=' + ratio + ',maximum-scale=' + ratio + ',minimum-scale=' + ratio + ',user-scalable=no,target-densitydpi=device-dpi,minimal-ui')
-    document.getElementsByTagName('head')[0].appendChild(meta)
-    document.getElementById(reactDomName).style.margin = '0 auto'
-    document.getElementById(reactDomName).style.width = width + 'px'
+    if (process.browser) {
+        const WIDTH = width
+        const ratio = screen.width / WIDTH
+        const meta = document.createElement('meta')
+        meta.setAttribute('name', 'viewport')
+        meta.setAttribute('content', 'width=' + WIDTH + ',initial-scale=' + ratio + ',maximum-scale=' + ratio + ',minimum-scale=' + ratio + ',user-scalable=no,target-densitydpi=device-dpi,minimal-ui')
+        document.getElementsByTagName('head')[0].appendChild(meta)
+        document.getElementById(reactDomName).style.margin = '0 auto'
+        document.getElementById(reactDomName).style.width = width + 'px'
+    }
 }
 
 export default scale
